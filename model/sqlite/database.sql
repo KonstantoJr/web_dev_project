61ba070e7de2eaa7e0573123cf3a07adaafc4aca
KonstantoJr <konstantokon29@gmail.com>
2022-06-01 00:24:17 +0300

    db creation

---
+++ model/sqlite/database.sql
@@ -0,0 +1,30 @@
+CREATE TABLE 'event'(
+    'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+    'name' TEXT NOT NULL,
+    'description' TEXT NOT NULL,
+    'total_seats' INTEGER NOT NULL,
+    'organizer' TEXT NOT NULL,
+    'admin_id' INTEGER NOT NULL,
+    'start_date' TEXT NOT NULL,
+    'img' BLOB NOT NULL,
+    'take_part' TEXT NOT NULL,
+    'phone' TEXT NOT NULL,
+    FOREIGN KEY('admin_id') REFERENCES 'admin'('id') ON DELETE CASCADE
+)
+
+CREATE TABLE 'admin'(
+    'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+    'username' TEXT NOT NULL,
+    'password' TEXT NOT NULL
+)
+
+CREATE TABLE 'reservation'(
+    'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+    'event_id' INTEGER NOT NULL,
+    'date' TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    'name' TEXT NOT NULL,
+    'phone' TEXT NOT NULL,
+    'number_of_seats' INTEGER NOT NULL,
+    'email' TEXT NOT NULL,
+    FOREIGN KEY('event_id') REFERENCES 'event'('id') ON DELETE CASCADE,
+)
