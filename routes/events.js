75942e2114c7eff9dfdab8502afb8ab0d46e13b2
kontman <pkontos00@gmail.com>
2022-06-01 23:20:38 +0300

    navbar prin kai tora events list

--- routes/events.js
+++ routes/events.js
@@ -11,7 +11,16 @@ router.use((req, res, next) => {
 
 router.get('/', async (req, res) => {
     // console.log(req.session)
-    res.render('events', { layout: 'bootstrap', style: "events.css", title: "Events", script: "events.js",userId: req.session.loggedUserId })
+    res.render('events', { layout: 'bootstrap', style: "events.css", title: "Events", script: "events.js",userId: req.session.loggedUserId,
+     events:[
+         {  
+             id : 1,
+             title:"malakies", 
+             date:"30/06/2022",
+             description: "irvl"
+            }
+        ]
+    })
 });
 
 
