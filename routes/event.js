e7b835d7db152eff7486aedec827e39eaa856195
kontman <pkontos00@gmail.com>
2022-06-02 03:44:12 +0300

    fill database & make events page

--- routes/event.js
+++ routes/event.js
@@ -10,8 +10,14 @@ router.use((req, res, next) => {
 
 
 router.get('/', async (req, res) => {
-    res.render('event', { layout: 'main', style: "event.css", title: "Event", script: "event.js" , event:"test"})
+    res.render('event', { layout: 'main', style: "event.css", title: "Event", script: "event.js" , userId: req.session.loggedUserId,
+    eventTitle:"test", eventDate: "30/05/2022", eventPlace: "Royal Theater", eventImg: "url"})
 });
 
 
 module.exports = router;
+
+
+// function myFunction() {
+//     console.log("aaaa");
+// }
---
+++ routes/home.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const express = require('express');
+const controller = require('../controllers/home');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+    next();
+});
+
+
+router.get('/', async (req, res) => {
+    // console.log(req.session)
+    res.render('home', { layout: 'main', style: "home.css", title: "Home", script: "home.js",userId: req.session.loggedUserId,})});
+
+
+
+
+
+module.exports = router;
